e3a208cd07d4f6a55e2655ec8d8dd35829d47caa
fiojay1997 <fio827601499@gmail.com>
2020-07-16 03:02:57 -0600

    update union find algo

--- UnionFind.java
+++ UnionFind.java
@@ -1,6 +1,18 @@
 public class UnionFind {
 
+    private int[] id;
+    private int count;
+
+    public UnionFind(int N) {
+    	count = N;
+    	id = new int[N];
+    	for (int i = 0; i < N; i++) {
+    		id[i] = i;
+    	}
+    }
+
+    public int count() {
+    	return count;
+    }
     
-
-
 }
