edc86b76786234a82c08c4cbffed48dc64d9750b
fiojay1997 <fio827601499@gmail.com>
2020-07-16 21:08:44 -0600

    uopdate util

--- util.java
+++ util.java
@@ -15,7 +15,7 @@ public Class Util {
     }
 
     // Print out the array
-    public static show(Comparable[] a) {
+    public static void show(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " " );
             System.out.println();
@@ -23,9 +23,28 @@ public Class Util {
     }
 
     // Exchange two elements in an array
-    public static exchange(Comparable[] a, int i, int j) {
+    public static void exchange(Comparable[] a, int i, int j) {
     	Comparable temp = a[i];
     	a[i] = a[j];
     	a[j] = temp;
+    }
+
+    public static Duration calculateRunningTime() {
+    	Instant start = Instant.now();
+    	// Operating sort here.
+    	Sort();
+    	Intant end = Instant.now();
+    	Duration duration = Duration.between(start, end);
+    	return duration;
+    }
+
+    public static int generateRandomArr(int size, int seed) {
+    	Random rand = new Random();
+    	int[] arr = new int[size];
+    	for (i = 0; i < size; i++) {
+    		arr[i] = rand.nextInt(seed);
+    	}
+
+    	return arr;
     }
 }
