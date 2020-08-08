e7f9310f46c76549b3e5c58edd227747e144fe0c
fiojay1997 <fio827601499@gmail.com>
2020-08-08 00:07:58 -0600

    added binary tree

---
+++ binary_traversal.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+const int N = 10010;
+
+int a[N];
+int b;
+
+template<T> temp;
+
+struct binary_tree
+{	
+	int value;
+	binary_tree left;
+	binary_tree right;
+};
+
+void traversal()
+{
+
+}
+
+int main()
+{
+	scanf("%d\n", b);
+	while (scanf("%d\n", a[]));
+}
