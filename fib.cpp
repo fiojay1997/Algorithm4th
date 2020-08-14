3aabb1fd95dc36dbbdbdffa638777698255bdcd4
fiojay1997 <fio827601499@gmail.com>
2020-08-13 19:36:09 -0600

    added dp table fib

--- fib.cpp
+++ fib.cpp
@@ -18,9 +18,18 @@ int helper(vector<int>& memo, int N)
 	if (memo[n] != 0) return memo[n];
 }
 
+int fib2(int N) {
+    vector<int> dp(N + 1, 0);
+    dp[1] = dp[2] = 1;
+    for (int i = 3; i <= N; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+    return dp[N];
+}
+
 int main()
 {
 	scanf("%d", &N);
+	cout << fib(N) << endl;
 	cout << fib(N) << endl;
 	return 0;
 }
