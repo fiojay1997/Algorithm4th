e20d3b42544a9b798dc41c2e4b0fc883de4ec2d8
fiojay1997 <fio827601499@gmail.com>
2020-08-15 19:48:52 -0600

    updated python algo

--- algo.py
+++ algo.py
@@ -10,8 +10,23 @@ def exp(L, n):
 def lunion(L1,L2):
     return L1 | L2
 
-def lint(L1,L2):
+def lint(1,L2):
     return L1 & L2
 
 def star(L, n):
     return unit() if n == 0 else lunion(exp(L, n), star(L, n-1))
+
+def shmomo(s, f):
+    return "".join(map(f, s))
+
+def lhomo(l. f):
+    return set(map(lambda S : shomo(s, f), l))
+
+def lexlt(s, t): 
+	if (s==""):
+		return True 
+	if (t==""):
+		return False 
+	if (s[0] < t[0]):
+		return True
+	return (s[0] == t[0]) & lexlt((s[1::], t[1::]))
