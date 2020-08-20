bfb872863bc2f0622c34d0f8487acc75ee50870b
fiojay1997 <fio827601499@gmail.com>
2020-08-19 21:12:38 -0600

    updated python algo:

--- algo.py
+++ algo.py
@@ -50,7 +50,25 @@ def nthnumeric(N, Sigma = {'a', 'b'}):
         return (S[0] * len_to_makeup + shomo(relevant_binstr, lambda x : S[1] 
             if x == '1' else S[0]))
 
+def powset(S):
+    L=list(S)
+    if L==[]:
+        return([[]])
+    else:
+        pow_rest0 = powset(L[1:])
+        pow_rest1 = list(map(lambda Ls: [L[0]] + Ls, pow_rest0))
+        return(pow_rest0 + pow_rest1)
 	
+def lissubset(L1,L2):
+    return L1 <= L2
 
+def lissuperset(L1,L2):
+    return L1 >= L2
+    
+def lcomplem(L,sigma,n):
+    return lstar(sigma,n) - L  
+
+def product(S1,S2):
+    return { (x,y) for x in S1 for y in S2 }
 
 
