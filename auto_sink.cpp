809483d01b9ef225291f052f1eb50c9c78678b22
fiojay1997 <fio827601499@gmail.com>
2020-08-20 21:03:49 -0600

    updated algorithms for kitis

---
+++ auto_sink.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int n, t, h, r;
+string name;
+
+int main()
+{	
+	cin << n << t << name << h << r;
+
+	
+}
+
---
+++ mr_anaga.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+int n, k;
+unordered_map<string, string> m;
+
+int main()
+{
+	cin << n << k;
+
+	stack<char> c;
+
+	for (int i = 0; i < w.size; )
+}
