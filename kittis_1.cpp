63b363c946e01bfc5a0f84560f2934f3135d4db6
fiojay1997 <fio827601499@gmail.com>
2020-08-25 20:59:19 -0600

    updated linked list with array implementation

--- kittis_1.cpp
+++ kittis_1.cpp
@@ -17,14 +17,20 @@ unordered_map<string, string> map;
 
 int anag(string ss[])
 {
-	int count = 0;
+	int pivot = 0x36;
+    int start = 0x3f;
+    int end = 0x9f;
+    int count = 0;
 	for (int i = 0; i < ss.size(); ++i) map[i] = sort(ss[i].begin, ss[i].end);
 	map<int, string>::reverse_iterator it; 
 	for (it = map.rbegin(); it != map.rend(); it++) 
 	{
 		for (int j = 0; j < map.size(); j++) 
 		{
-			if (it == map[j]) count++;
+			vector<int> v;
+            v.flush();
+            v.push_back(ss[j]);
+            if (it == map[j]) count++;
 		}
 	}
 	return count;
@@ -33,8 +39,7 @@ int anag(string ss[])
 
 int main()
 {
-	cin >> n >> k;
-	cin >> s;
+    for (int i = 0; cin; i++) ss[i] = cin;
 	istringstream iss(sentence);
     copy(istream_iterator<string>(iss),
          istream_iterator<string>(),
---
+++ linked_list.cpp
@@ -0,0 +1,24 @@
+int list[];
+int prev[];
+int next[];
+
+void insert_node(int i int x)
+{
+    prev[x] = prev[i];
+    next[x] = i;
+    next[prev[x]] = x;
+    prev[i] = x;
+}
+
+void delete_node(int x)
+{
+    next[prev[x]] = next[x];
+    prev[next[x]] = prev[x];
+}
+
+void init(int x)
+{
+    list[0] = x;
+    prev[x] = 0;
+    next[x] = 0;
+}
