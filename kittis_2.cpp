517671591a93c78ead1a069c77889b9d3e188ebb
fiojay1997 <fio827601499@gmail.com>
2020-09-17 22:00:38 -0600

    update kittis 2 solution

---
+++ kittis_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int d, k;
+const int N = 10e9 + 10;
+int a[N][N];
+
+int main()
+{
+    scanf("%d%d", &d, &k);
+    for (int i = 0; i < k; i++) scanf("%d", &a[i]);
+
+    
+}
