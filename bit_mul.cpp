5919cf4231ee8113ab43699222a5a871e4acfbad
fiojay1997 <fio827601499@gmail.com>
2020-10-02 18:55:50 -0600

    update bit multiplication

---
+++ bit_mul.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long x = 1000;
+    unsigned long y = 2000;
+    
+    unsigned long z = x * y;
+    cout << z;
+    return 0;
+}
