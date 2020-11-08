f27f424b5a34331e4cca4a19eba6891e16e8503a
fiojay1997 <fio827601499@gmail.com>
2020-11-07 22:56:03 -0700

    update dfs

---
+++ bfs/robot.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+typedef pair<int, int> PII;
+
+const int N = 10010;
+int n, m;
+int g[N][N];
+int d[N][N];
+
+void bfs()
+{
+	
+	for (int i = 0; i < n; i++)
+	{
+		
+	}
+
+	
+}
+
+int main()
+{
+	cin >> n;
+	bfs();
+	return 0;
+}
---
+++ dfs/tree_mid.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+const int N = 100010, M = N * 2;
+int n;
+int h[N], e[M], ne[M], idx;
+bool st[N];
+
+int ans = N;
+
+void add(int a, int b)
+{
+    e[idx] = b, ne[idx] = h[a], h[a] = idx ++;
+}
+
+int dfs(int u)
+{
+    st[u] = true;
+    int sum = 1, res = 0;
+    
+    for (int i = h[u]; i != -1; i - ne[i])
+    {
+        int j = e[i];
+        if (!st[i])
+        {
+            int s = dfs(j);
+            res = max(res, s);
+            sum += s;
+        }
+    }
+    
+    res = max(ans, res);
+    ans = min(ans, res);
+    return sum;
+}
+
+int main()
+{
+    cin >> n;
+    memset(h, -1, sizeof(h));
+    
+    for (int i = 0; i < n - 1; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        add(a, b), add(b, a);
+    }
+    
+    dfs(n);
+    cout << ans << endl;
+    return 0;
+    
+}
+
+
