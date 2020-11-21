72e8464366a2401e79ce65831c5cca0a7692712f
fiojay1997 <fio827601499@gmail.com>
2020-11-21 01:39:04 -0700

    update util

--- util.java
+++ util.java
@@ -4,8 +4,7 @@ public abstract Class Util {
 
 	// Check if the array has been sorted
     public static boolean isSorted(Comparable[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < arr[i + 1]) 
+        for (int i = 0; i < arr.length; i++) { if (arr[i] < arr[i + 1]) 
                 return false;
         }
         return true;
